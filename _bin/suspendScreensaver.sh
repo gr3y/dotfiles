a54a5867125cf126970d35f36a67aaa467ad72d3
Andreas Persson <andreas@greyscale.se>
2011-10-03 19:05:04 +0200

    Small script to suspend screensaver as long as the terminal is running.

---
+++ _bin/suspendScreensaver.sh
@@ -0,0 +1,30 @@
+# !/bin/bash
+#
+#    @@@@@@@@  @@@@@@@   @@@@@@@@  @@@ @@@   @@@@@@    @@@@@@@   @@@@@@   @@@       @@@@@@@@
+#   @@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@ @@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@       @@@@@@@@
+#   !@@        @@!  @@@  @@!       @@! !@@  !@@       !@@       @@!  @@@  @@!       @@!
+#   !@!        !@!  @!@  !@!       !@! @!!  !@!       !@!       !@!  @!@  !@!       !@!
+#   !@! @!@!@  @!@!!@!   @!!!:!     !@!@!   !!@@!!    !@!       @!@!@!@!  @!!       @!!!:!
+#   !!! !!@!!  !!@!@!    !!!!!:      @!!!    !!@!!!   !!!       !!!@!!!!  !!!       !!!!!:
+#   :!!   !!:  !!: :!!   !!:         !!:         !:!  :!!       !!:  !!!  !!:       !!:
+#   :!:   !::  :!:  !:!  :!:         :!:        !:!   :!:       :!:  !:!   :!:      :!:
+#    ::: ::::  ::   :::   :: ::::     ::    :::: ::    ::: :::  ::   :::   :: ::::   :: ::::
+#    :: :: :    :   : :  : :: ::      :     :: : :     :: :: :   :   : :  : :: : :  : :: ::
+#
+#     .: FILE:          suspendScreensaver.sh
+#     .: CREATED:       2011-10-03 19:01
+#     .: AUTHOR:        Andreas Persson (greyscale, grey)
+#     .: MAIL:          <andreas(at)greyscale(dot)se>
+#     .: VERSION:       1.0
+#     .: DESCRIPTION:   Suspends screensaver as long as terminal is running.
+#
+#  __________________________________________________________________________________________
+#   This file is distributed "as it is", like it? Then use it in whole or parts of it but be
+#   minded that it comes with absolutely no warranty. But then again you got it for free..
+#  __________________________________________________________________________________________
+#
+
+curId=`xdotool getwindowfocus`
+
+xdg-screensaver suspend ${curId}
+
